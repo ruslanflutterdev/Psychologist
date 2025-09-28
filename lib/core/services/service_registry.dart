da93d397a17c8137151158da975eb57fb2ea1af0
ruslanflutterdev <rusrus1984_8@mail.ru>
2025-09-28 20:31:22 +0500

    добавил новый сервис QuestService и его мок-реализация MockQuestService, эмуляция POST /assign-quest с payload {childid, difficutly, assignedAt, добавил логику задержки и флаг failNetwork для проверки ошибок, в ServiceReqistry зарегестрировал новый guest-сервис

--- lib/core/services/service_registry.dart
+++ lib/core/services/service_registry.dart
@@ -1,3 +1,5 @@
+import 'package:heros_journey/core/services/mock_quest_service.dart';
+import 'package:heros_journey/core/services/quest_service.dart';
 import 'package:heros_journey/features/auth_registration/viewmodel/services/auth_service.dart';
 import 'package:heros_journey/features/auth_registration/viewmodel/services/mock_auth_service.dart';
 import 'package:heros_journey/features/child_screen/viewmodel/services/child_service.dart';
@@ -6,9 +8,11 @@ import 'package:heros_journey/features/child_screen/viewmodel/services/mock_chil
 class ServiceRegistry {
   static late AuthService auth;
   static late ChildService child;
+  static late QuestService quest;
 
   static void initMocks() {
     auth = MockAuthService();
     child = MockChildService();
+    quest = MockQuestService();
   }
 }
