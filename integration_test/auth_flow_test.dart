08ee1311cdb272b4e1633755f3b8b348f6c8e551
ruslanflutterdev <rusrus1984_8@mail.ru>
2025-10-23 15:17:37 +0500

    исправил отображение иконок в веб версии

--- integration_test/auth_flow_test.dart
+++ integration_test/auth_flow_test.dart
@@ -2,10 +2,8 @@ import 'dart:nativewrappers/_internal/vm/bin/vmservice_io.dart' as app;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 
 void main() {
-  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('Auth flows', () {
     setUpAll(() async {
